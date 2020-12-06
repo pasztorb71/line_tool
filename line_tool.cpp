98feeb2928e82286c8bea116306bbc3a67da5732
Pásztor Bertalan <pasztor.bertalan@autsoft.hu>
2020-12-06 15:29:40 +0100

    Separate header file

--- line_tool.cpp
+++ line_tool.cpp
@@ -1,31 +1,7 @@
 #include <windows.h>
 #include <stdio.h>
+#include "line_tool.h"
 
-#define FILENAME_MAXLEN 260
-
-int file_open(char* fname_i, const char* open_mode_i, FILE** fhandler_o, char* errmsg_io, int errmsg_maxlen);
-int fill_new_record(char* line);
-long find_record(char* line);
-void free_records(void);
-void print_records(int o_mode, int to_sort);
-int cmpfunc_sort(const void* a, const void* b);
-
-enum mode {
-	count = 0,
-	unique = 1,
-	not_unique = 2,
-};
-
-struct s_line {
-	char* line;
-	unsigned long cnt;
-};
-struct s_line* lines = NULL;
-long lines_cnt = 0;
-char fname_i[260] = { 0 };
-char fname_o[260] = { 0 };
-FILE* fp_input;
-FILE* fp_output;
 
 //------------------------------------------------------------------------------------------------------------------
 
